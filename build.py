287ae48353e9fd26652e053715bdba246ccc7c96
Vladas Zakrevskis <146100@gmail.com>
2018-10-08 11:15:42 +0300

    automatic build

---
+++ build.py
@@ -0,0 +1,26 @@
+import os
+import sys
+sys.path.append('utils')
+import Args
+import Shell
+
+project_name = "verilog_test"
+
+def build():
+    Shell.run_string("yosys -q -p \"synth_ice40 -blif " + project_name + ".blif\" top.v")
+    Shell.run_string("arachne-pnr -d 8k -p " +
+                     project_name + ".pcf " +
+                     project_name + ".blif -o " +
+                     project_name + ".asc")
+
+    Shell.run_string("icepack " +
+                     project_name + ".asc " +
+                     project_name  + ".bin")
+
+def run():
+    Shell.run_string("icoprog -p < " + project_name + ".bin")
+
+build()
+
+if Args.run:
+    run()
